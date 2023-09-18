fd96c9340e79e97630f277ae81b4d721e5f116bc
katheprc <yaelvaldez@gmail.com>
2023-09-18 18:32:31 -0300

    dockerized microservice practice

---
+++ src/main/java/com/katheprc/MicroservicePractice/MicroservicePracticeApplication.java
@@ -0,0 +1,21 @@
+package com.katheprc.MicroservicePractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@SpringBootApplication
+public class MicroservicePracticeApplication {
+
+	@GetMapping("/weatherForecast")
+	public String getMessage() {
+		return "Today is a great sunny day for a Java service!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/katheprc/MicroservicePractice/MicroservicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katheprc.MicroservicePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
